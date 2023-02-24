51caedabb9220a13a4e8c7b3b599fc2b40840ff2
Hemchandrasuyal <hem.suyal@incedoinc.com>
2023-02-24 13:28:22 +0530

    first commit

---
+++ src/modules/Employee/component/Main.js
@@ -0,0 +1,101 @@
+import React, { useEffect, useState } from "react";
+import "./Main.css";
+import { experimentalStyled as styled } from "@mui/material/styles";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import timesheetData from "../../../assets/data/showtimesheettoemployee.json";
+import approvedData from "../../../assets/data/showtimesheettoemployeebasedonaprroval.json";
+import logo from "../../../assets/Capture.png";
+import {useNavigate} from "react-router-dom";
+import {useLocation} from "react-router-dom";
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: "#043465",
+  ...theme.typography.body2,
+  padding: theme.spacing(2),
+  color: "white",
+  height: "180px",
+}));
+
+const Main = (props) => {
+  const [count, setCount] = useState({ approved: 0, rejected: 0, pending: 0 });
+  const [data, setData] = useState(timesheetData);
+  const navigate=useNavigate();
+  const location=useLocation();
+ console.log('location',location)
+  useEffect(() => {
+    setCount({ ...count, approved: approvedData.length });
+  }, []);
+
+  const handleClick = (e) => {
+    if (e.target.id == "approved") {
+      setData(approvedData);
+    }
+  };
+
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${logo})`,
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "top center",
+        backgroundSize: "400px",
+        backgroundPositionY: "60px",
+      }}
+    >
+<div></div>
+      <Grid
+        container
+        spacing={{ xs: 2, md: 3 }}
+        columns={{ xs: 4, sm: 8, md: 12 }}
+        style={{ marginTop: "20px" }}
+      >
+        <Grid item xs={10}>
+          <div className="status" onClick={handleClick} id="approved">
+            Approved {count.approved}
+          </div>
+          <div className="status">Rejected {count.rejected}</div>
+          <div className="status">Pending {count.pending}</div>
+        </Grid>
+        <Grid item xs={2}>
+          <div className="week-status">Dropdown {count.pending}</div>
+        </Grid>
+      </Grid>
+
+      <Box sx={{ flexGrow: 1 }} style={{ margin: "180px 20px 0px 20px" }}>
+        <Grid
+          container
+          spacing={{ xs: 3, md: 3 }}
+          columns={{ xs: 4, sm: 8, md: 12 }}
+        >
+          {data.map((item) => (
+            <Grid item xs={2} sm={2} md={3}>
+              <Item>
+                <h2>{item.timesheetId}</h2>
+                <p>Project Name: {item.project.projectName}</p>
+                <p>Project Id:{item.project.project_id}</p>
+                <p>Manager Name: {item.project.employee.employee_name}</p>
+                <p>Remarks:</p>
+                <p>Status:</p>
+              </Item>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+    </div>
+  );
+};
+
+export default Main;
+
+// {data.map((item) => (
+//   <Grid item xs={2} sm={2} md={3} key={item.project_id}>
+//     <Item>
+//       <h2>{item.timesheetId}</h2>
+//       <p>Project Name: {item.project.projectName}</p>
+//       <p>Proj,ect Id:{item.II - PROJ - 00010}</p>
+//       <p>Manager Name: {item.employee_name}</p>
+//       <p>Status: {}</p>
+//     </Item>
+//   </Grid>
+// ))}
---
+++ src/modules/Employee/container/Employee.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+import Navbar from "../../Navbar/Navbar";
+import Main from "../component/Main";
+import Footer from "../../Footer/Footer";
+
+function Employee() {
+  return (
+    <>
+      <Navbar />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+export default Employee;
